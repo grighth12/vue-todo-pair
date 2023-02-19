5cd6a8f90684ea7a655b8b0beed7e8d847514f66
hanna <hanna@grepp.co>
2023-02-19 19:45:26 +0900

    feat: trim, validation 추가

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+const errorMessage = ref("");
 const value = ref("");
 const todos = ref<{ id: number; value: string }[]>([]);
 
 const onSubmit = () => {
+  const todo = value.value.trim();
+
+  if (!todo) {
+    errorMessage.value = "할 일을 입력해주세요.";
+    return;
+  }
+
   todos.value.push({
     id: new Date().getUTCSeconds(),
-    value: value.value,
+    value: todo,
   });
+
   value.value = "";
+  errorMessage.value = "";
 };
 </script>
 
@@ -18,6 +28,7 @@ const onSubmit = () => {
     <input type="text" v-model="value" />
     <button type="submit">제출하기</button>
   </form>
+  <div class="error">{{ errorMessage }}</div>
 
   <ul>
     <li v-for="{ id, value } in todos" :key="id">
@@ -26,4 +37,8 @@ const onSubmit = () => {
   </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+.error {
+  color: #f00;
+}
+</style>
