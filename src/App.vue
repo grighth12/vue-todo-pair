a22e83f3e4e76adebc0992dd043ac6495706c798
hanna <hanna@grepp.co>
2023-02-19 19:33:32 +0900

    feat: 투두 항목 추가

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref("");
+const todos = ref<string[]>([]);
+
+const onSubmit = () => {
+  todos.value.push(value.value);
+  value.value = "";
+};
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="value" />
+    <button type="submit">제출하기</button>
+  </form>
+
+  <div>{{ todos.join() }}</div>
+</template>
+
+<style scoped></style>
